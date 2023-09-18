d65547a27cd05afe53f7264ca985c5af5fb3b959
Scott Stephens <scott@nuruq.com>
2023-09-18 09:22:08 -0700

    checkpoint after shadcn blew away my styles

---
+++ app/(root)/page.tsx
@@ -0,0 +1,12 @@
+import { UserButton } from '@clerk/nextjs';
+
+function Home() {
+  return (
+    <>
+        <h1 className="head-text text left">Home</h1>
+
+    </>
+  );
+}
+
+export default Home;
